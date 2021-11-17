1c662678c30dc1a73ad6560ef833797cbbc2f35b
junombrown <90719489+junombrown@users.noreply.github.com>
2021-11-17 11:30:28 +0100

    version4

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ var page = $("#info_page");
 var page_project = $("#plaster");
 var all_projects = $(".page_project");
 var all_pages = $(".page_project, .move");
+
+
 $(document).ready(function() {
-    console.log("website developed by Marie Verdeil")
-    console.log("https://verdeil.net")
-        //if click on button, open the right page 
+
+    //NAVIGATION 
+    //   (for pages) 
+
+    //if click on button, open the right page 
     $(".nav_box").on("click", function(event) {
 
         //closing the project pages
@@ -17,20 +21,14 @@ $(document).ready(function() {
             $(all_pages).removeClass("open")
 
         } else {
-
+            $(page).removeClass("open");
             //toggle betwen info and project
-            // $(page).toggleClass("open");
-            newpage = $(this).attr("href");
-            if (page != newpage) {
-                $(page).removeClass("open");
-                page = $(this).attr("href");
-            }
-
+            page = $(this).attr("href");
             $(page).toggleClass("open");
         }
 
     });
-
+    //   (for projects) 
     $(".icon").on("click", function(event) {
         $('#projects_page').toggleClass("open")
         page_project = $(this).attr("href");
@@ -38,6 +36,10 @@ $(document).ready(function() {
 
 
     });
+
+    //CREDITS 
+    console.log("website developed by Marie Verdeil")
+    console.log("https://verdeil.net")
 
 
 
